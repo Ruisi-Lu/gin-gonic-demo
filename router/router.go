f314d0b8f1f330202c88b9862fb6a501ac1e572f
Ruisi <lhc03952@gmail.com>
2019-10-25 14:52:24 +0800

    新增變數傳遞用法

--- router/router.go
+++ router/router.go
@@ -10,14 +10,19 @@ func Init() {
 	r := gin.Default()
 	// 設定群組 /demo   eg. http://0.0.0.0/demo/
 	demo := r.Group("/demo")
-	{
 		// 子目錄 eg. http://0.0.0.0/demo/123
 		demo.GET("/", handlers.Welcome)
 		demo.GET("123", handlers.HelloPage)
 		demo.GET("a", handlers.HelloPage2)
+		// 參數傳遞 (*或:)
+		// 必給參數 不給會404
 		demo.GET("/hello/:name", handlers.HelloPage3)
-		demo.POST("/hello/:name", handlers.HelloPage4)
-	}
+		// 可不給參數
+		demo.GET("/hello2/*name", handlers.HelloPage3)
+		// post demo
+		demo.POST("/posthello/:name", handlers.HelloPage4)
+
+
 	r.Run(":80")
 
 }
