c5902ff0de07aaed1aede7bdcf6a12d579db1a9f
Ruisi <lhc03952@gmail.com>
2019-10-31 14:19:13 +0800

    加入註解刪除不必要func

--- handlers/login.go
+++ handlers/login.go
@@ -4,12 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
-
+// Login 簡單BasicAuth
 func Login(context *gin.Context) {
 	context.String(http.StatusOK, "welcome login demo")
-}
-
-func Loginwelcome(context *gin.Context) {
-	name := context.Param("name")
-	context.String(http.StatusOK, "welcome login demo %s", name)
 }
